7c954d2eff4b2fda26e2987518253d13304e2941
Edward Chen <edwjchen@gmail.com>
2020-09-15 16:58:47 -0700

    updating movement scripts and added directional components and flash

---
+++ Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour {
+
+    public int rotateSpeed;
+    public int movementSpeed;
+   
+    public enum Direction {North=0, NorthEast=1, East=2, SouthEast=3, South=4, SouthWest=5, West=6, NorthWest=7};
+    public Direction currDirection;
+    public int[][] directions = new int[][] {
+        new int[] {0,1},
+        new int[] {1,1},
+        new int[] {1,0},
+        new int[] {1,-1},
+        new int[] {0,-1},
+        new int[] {-1,-1},
+        new int[] {-1,0},
+        new int[] {-1,1}
+    };
+
+
+	// Use this for initialization
+	void Start () {
+        currDirection = Direction.North;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        Move();
+	}
+
+    private void Move() {
+        int x = 0;
+        int y = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0, 0);
+            x += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0, 0);
+            x -= 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(0, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime, 0);
+            y += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.Translate(0, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime, 0);
+            y -= 1;
+        }
+
+        if (x != 0 || y != 0) {
+            FacingDirection(x, y);
+        }
+    }
+
+    private void FacingDirection(int x, int y) {
+        for (int i = 0; i < directions.Length; i++) {
+            if (directions[i][0] == x && directions[i][1] == y) {
+                currDirection = (Direction) i;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BoPeep.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace TimeArithmetic {
+    public class BoPeep : BaseMovement {
+
+        public int flashRange;
+        public int flashCD; // in seconds
+        private DateTime lastFlashTime = DateTime.Now;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            base.Update();
+            // UnityEngine.Debug.Log("position: " + transform.position);
+            // UnityEngine.Debug.Log("direction: " + base.currDirection);
+            if (Input.GetKey(KeyCode.D)) {
+                
+            }
+
+            if (Input.GetKey(KeyCode.F) && canFlash()) {
+                Flash();
+            }
+        }
+
+
+        bool canFlash() {
+            if (lastFlashTime.AddSeconds(flashCD) <= DateTime.Now) {
+                return true;
+            }
+            return false;
+        }
+
+
+        // Ability to Flash over Walls 
+        void Flash() {
+            lastFlashTime = DateTime.Now;
+            int[] flashDirection = directions[(int)base.currDirection];
+
+            for (int i = 0; i < flashDirection.Length; i++) {
+                flashDirection[i] *= flashRange;
+            }
+            UnityEngine.Debug.Log(transform.position);
+            UnityEngine.Debug.Log(flashDirection);
+            UnityEngine.Debug.Log(transform.position);
+
+            Vector3 vector = new Vector3(flashDirection[0], flashDirection[1], 0);
+
+            transform.position += vector;
+        }
+
+        void Catch() {
+
+        }
+    }
+}
