d24fc302291de1cb050d3012808b173a15037d4c
Caroline Sih <sih.caroline@gmail.com>
2020-09-14 17:10:37 -0700

    initial 2D commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 camPos = player.transform.position;
+
+        camPos.y += 2.5f;
+
+        this.transform.position = camPos;
+	}
+}
